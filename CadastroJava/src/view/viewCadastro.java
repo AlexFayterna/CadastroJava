443b44925bb2c6a5573d5588f971bf6b81bcac24
AlexFayterna <alex_fayterna@estudante.sc.senai.br>
2017-10-10 22:00:01 -0300

    Modificado CategoriaDAO, Categoria e viewCadastro para listagem de categorias dentro de [ComboBoxCategoria]

--- CadastroJava/src/view/viewCadastro.java
+++ CadastroJava/src/view/viewCadastro.java
@@ -5,6 +5,9 @@
  */
 package view;
 
+import model.bean.Categoria;
+import model.dao.CategoriaDAO;
+
 /**
  *
  * @author Administrador
@@ -13,11 +16,20 @@ public class viewCadastro extends javax.swing.JFrame {
 
     /**
      * Creates new form viewCadastro
+     * método construtor
      */
     public viewCadastro() {
         initComponents();
+        preencherComboBoxCategorias();
     }
 
+    private void preencherComboBoxCategorias(){
+        CategoriaDAO catDao = new CategoriaDAO();
+        for(Categoria cat:catDao.findAll()){
+            jComboBoxCategorias.addItem(cat);
+        }
+    }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
